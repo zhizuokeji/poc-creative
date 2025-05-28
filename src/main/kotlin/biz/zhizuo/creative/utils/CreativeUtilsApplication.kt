b55e91d40fe38c2e328649dc7ce868d526e6b4d8
Ralph Wang <traveller@163.com>
2025-05-28 20:54:07 +0800

    fix: 创建默认项目

---
+++ src/main/kotlin/biz/zhizuo/creative/utils/CreativeUtilsApplication.kt
@@ -0,0 +1,11 @@
+package biz.zhizuo.creative.utils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreativeUtilsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreativeUtilsApplication>(*args)
+}
